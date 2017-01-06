8c25b74e0158a058325191267f5ed927ee5d7c23
Program <Program@DarrylHodeMacBook-Pro.local>
2017-01-06 21:26:14 +0800

    Added eric.h so that EH knows where & how to work

---
+++ eric.h
@@ -0,0 +1,24 @@
+struct Eric
+{
+    //EH, please finish the struct.
+    Eric()
+    {
+        //TODO
+    }
+    ~Eric()
+    {
+        //TODO
+    }
+    void render()
+    {
+        //TODO
+    }
+    tuple<vector<int>,int,bool> step(int action)
+    {
+        //TODO
+    }
+    vector<int> reset()
+    {
+        //TODO
+    }
+};
