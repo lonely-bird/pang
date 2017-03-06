ba2b78df99161c5b029b3719735df78721a971e0
John Chen <johnchen902@gmail.com>
2017-03-06 17:19:33 +0800

    Use explicit pointer to store data.
    This is required do some low level stuff better
    such as OpenACC.

--- learning.cpp
+++ learning.cpp
@@ -1,217 +1,314 @@
 #include "learning.h"
 #include <cmath>
+#include <algorithm>
 namespace learning {
-template<typename T>
-Matrix<T> &operator += (Matrix<T> &lhs, const Matrix<T> &rhs) {
-    assert(lhs.rows() == rhs.rows());
-    assert(lhs.columns() == rhs.columns());
-    lhs.data() += rhs.data();
-    return lhs;
+Vector::Vector(unsigned size) :
+        m_size(size), m_data(new double[size]()) {
 }
-template<typename T>
-Matrix<T> operator + (const Matrix<T> &lhs, const Matrix<T> &rhs) {
-    Matrix<T> result = lhs;
-    result += rhs;
-    return std::move(result);
+Vector::Vector(const Vector &rhs) :
+        m_size(rhs.size()), m_data(new double[m_size]()) {
+    std::copy_n(rhs.data(), rhs.size(), data());
 }
-template<typename T>
-Matrix<T> &operator -= (Matrix<T> &lhs, const Matrix<T> &rhs) {
-    assert(lhs.rows() == rhs.rows());
-    assert(lhs.columns() == rhs.columns());
-    lhs.data() -= rhs.data();
-    return lhs;
+Vector::Vector(const std::vector<double> &rhs) :
+        m_size(rhs.size()), m_data(new double[m_size]()) {
+    std::copy(rhs.begin(), rhs.end(), data());
 }
-template<typename T>
-Matrix<T> operator - (const Matrix<T> &lhs, const Matrix<T> &rhs) {
-    Matrix<T> result = lhs;
-    result -= rhs;
-    return std::move(result);
+Vector &Vector::operator = (const Vector &rhs) {
+    if(this != &rhs) {
+        if(m_size != rhs.size()) {
+            m_size = rhs.size();
+            m_data.reset(new double[m_size]());
+        }
+        std::copy_n(rhs.data(), rhs.size(), data());
+    }
+    return *this;
 }
-template<typename T>
-Matrix<T> &operator *= (Matrix<T> &lhs, T rhs) {
-    lhs.data() *= rhs;
-    return lhs;
+Vector::~Vector() {
 }
-template<typename T>
-Matrix<T> operator * (const Matrix<T> &lhs, T rhs) {
-    Matrix<T> result = lhs;
-    result *= rhs;
-    return std::move(result);
+Vector::operator std::vector<double> () const {
+    return std::vector<double>(data(), data() + size());
 }
-template<typename T>
-std::valarray<T> operator * (const Matrix<T> &lhs,
-        const std::valarray<T> &rhs) {
-    assert(lhs.columns() == (int) rhs.size());
-    std::valarray<T> result(lhs.rows());
-    for(int i = 0; i < lhs.rows(); i++)
-        for(int j = 0; j < lhs.columns(); j++)
-            result[i] += lhs[i][j] * rhs[j];
-    return std::move(result);
+double Vector::operator [] (unsigned index) const {
+    assert(index < size());
+    return data()[index];
 }
-template<typename T>
-std::valarray<T> operator * (const std::valarray<T> &lhs,
-        const Matrix<T> &rhs) {
-    assert((int) lhs.size() == rhs.rows());
-    std::valarray<T> result(rhs.columns());
-    for(int i = 0; i < rhs.columns(); i++)
-        for(int j = 0; j < rhs.rows(); j++)
-            result[i] += lhs[j] * rhs[j][i];
-    return std::move(result);
+double &Vector::operator [] (unsigned index) {
+    assert(index < size());
+    return data()[index];
 }
-template<typename T>
-Matrix<T> operator * (const Matrix<T> &lhs, const Matrix<T> &rhs) {
-    assert(lhs.columns() == rhs.rows());
-    Matrix<T> result(lhs.rows(), rhs.columns());
-    for(int i = 0; i < lhs.rows(); i++)
-        for(int k = 0; k < lhs.columns(); k++)
-            for(int j = 0; j < rhs.columns(); j++)
-                result[i][j] += lhs[i][k] * rhs[k][j];
-    return std::move(result);
-}
-
-template<typename T>
-T inner_product(const std::valarray<T> &lhs, const std::valarray<T> &rhs) {
+Vector &operator += (Vector &lhs, const Vector &rhs) {
     assert(lhs.size() == rhs.size());
-    T result = 0;
-    for(int i = 0; i < (int) lhs.size(); i++)
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs[i] += rhs[i];
+    return lhs;
+}
+Vector &operator += (Vector &lhs, double rhs) {
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs[i] += rhs;
+    return lhs;
+}
+Vector &operator -= (Vector &lhs, const Vector &rhs) {
+    assert(lhs.size() == rhs.size());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs[i] -= rhs[i];
+    return lhs;
+}
+Vector &operator -= (Vector &lhs, double rhs) {
+    return lhs += -rhs;
+}
+Vector &operator *= (Vector &lhs, const Vector &rhs) {
+    assert(lhs.size() == rhs.size());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs[i] *= rhs[i];
+    return lhs;
+}
+Vector &operator *= (Vector &lhs, double rhs) {
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs[i] *= rhs;
+    return lhs;
+}
+Vector &operator /= (Vector &lhs, const Vector &rhs) {
+    assert(lhs.size() == rhs.size());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs[i] /= rhs[i];
+    return lhs;
+}
+Vector &operator /= (Vector &lhs, double rhs) {
+    return lhs *= 1.0 / rhs;
+}
+double sum(const Vector &lhs) {
+    double result = 0;
+    for(unsigned i = 0; i < lhs.size(); i++)
+        result += lhs[i];
+    return result;
+}
+double sum_square(const Vector &lhs) {
+    double result = 0;
+    for(unsigned i = 0; i < lhs.size(); i++)
+        result += lhs[i] * lhs[i];
+    return result;
+}
+double inner_product(const Vector &lhs, const Vector &rhs) {
+    assert(lhs.size() == rhs.size());
+    double result = 0;
+    for(unsigned i = 0; i < lhs.size(); i++)
         result += lhs[i] * rhs[i];
     return result;
 }
 
-template<typename T>
-Matrix<T> to_matrix(const std::vector<std::valarray<T>> v) {
-    if(v.empty())
-        return Matrix<T>(0, 0);
-    Matrix<T> result(v.size(), v[0].size());
-    for(int i = 0; i < result.rows(); i++)
-        for(int j = 0; j < result.columns(); j++)
-            result[i][j] = v[i][j];
-    return result;
+Matrix::Matrix(unsigned row, unsigned col) :
+        m_rows(row), m_columns(col), m_data(new double[row * col]()) {
 }
-template<typename T>
-std::valarray<T> to_valarray(const std::vector<T> v) {
-    return std::valarray<T>(v.data(), v.size());
+Matrix::Matrix(const Matrix &rhs) :
+        m_rows(rhs.rows()), m_columns(rhs.columns()),
+        m_data(new double[m_rows * m_columns]()) {
+    std::copy_n(rhs.data(), rhs.size(), data());
 }
-
-void Model::reset() {
-    m_w1 = 0;
-    m_w2 = 0;
+Matrix::Matrix(const std::vector<std::vector<double>> &rhs) :
+        m_rows(rhs.size()), m_columns(rhs.front().size()),
+        m_data(new double[m_rows * m_columns]()) {
+    for(unsigned i = 0; i < m_rows; i++)
+        std::copy(rhs[i].begin(), rhs[i].end(), (*this)[i]);
 }
-#if 0
-Model &Model::operator += (Model &lhs, const Model &rhs) {
-    lhs.w1() += rhs.w1();
-    lhs.w2() += rhs.w2();
+Matrix &Matrix::operator = (const Matrix &rhs) {
+    if(this != &rhs) {
+        unsigned old_size = m_rows * m_columns;
+        m_rows = rhs.rows();
+        m_columns = rhs.columns();
+        if(m_rows * m_columns != old_size)
+            m_data.reset(new double[m_rows * m_columns]());
+        std::copy_n(rhs.data(), rhs.size(), data());
+    }
+    return *this;
+}
+Matrix::~Matrix() {
+}
+Matrix::operator std::vector<std::vector<double>> () const {
+    std::vector<std::vector<double>> v;
+    v.reserve(rows());
+    for(unsigned i = 0; i < rows(); i++)
+        v.emplace_back((*this)[i], (*this)[i] + columns());
+    return std::move(v);
+}
+const double *Matrix::operator [] (unsigned row) const {
+    assert(row < rows());
+    return data() + row * columns();
+}
+double *Matrix::operator [] (unsigned row) {
+    assert(row < rows());
+    return data() + row * columns();
+}
+Matrix &operator += (Matrix &lhs, const Matrix &rhs) {
+    assert(lhs.rows() == rhs.rows());
+    assert(lhs.columns() == rhs.columns());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs.data()[i] += rhs.data()[i];
     return lhs;
 }
-#endif
+Matrix &operator -= (Matrix &lhs, const Matrix &rhs) {
+    assert(lhs.rows() == rhs.rows());
+    assert(lhs.columns() == rhs.columns());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs.data()[i] -= rhs.data()[i];
+    return lhs;
+}
+Matrix &operator *= (Matrix &lhs, double rhs) {
+    for(unsigned i = 0; i < lhs.size(); i++)
+        lhs.data()[i] *= rhs;
+    return lhs;
+}
+Vector operator * (const Matrix &lhs, const Vector &rhs) {
+    assert(lhs.columns() == rhs.size());
+    Vector result(lhs.rows());
+    for(unsigned i = 0; i < lhs.rows(); i++)
+        for(unsigned j = 0; j < lhs.columns(); j++)
+            result[i] += lhs[i][j] * rhs[j];
+    return std::move(result);
+}
+Vector operator * (const Vector &lhs, const Matrix &rhs) {
+    assert(lhs.size() == rhs.rows());
+    Vector result(rhs.columns());
+    for(unsigned i = 0; i < rhs.columns(); i++)
+        for(unsigned j = 0; j < rhs.rows(); j++)
+            result[i] += lhs[j] * rhs[j][i];
+    return std::move(result);
+}
+Matrix operator * (const Matrix &lhs, const Matrix &rhs) {
+    assert(lhs.columns() == rhs.rows());
+    Matrix result(lhs.rows(), rhs.columns());
+    for(unsigned i = 0; i < lhs.rows(); i++)
+        for(unsigned k = 0; k < lhs.columns(); k++)
+            for(unsigned j = 0; j < rhs.columns(); j++)
+                result[i][j] += lhs[i][k] * rhs[k][j];
+    return std::move(result);
+}
 
-constexpr int batch_size = 10; // every how many episodes to do a param update?
+void reset(Model &m) {
+    for(unsigned i = 0; i < m.w1().size(); i++)
+        m.w1().data()[i] = 0.0;
+    for(unsigned i = 0; i < m.w2().size(); i++)
+        m.w2()[i] = 0.0;
+}
+template<typename Engine>
+void randomize(Model &m, Engine &eng) {
+    std::normal_distribution<double> gauss;
+    for(unsigned i = 0; i < m.w1().rows(); i++)
+        for(unsigned j = 0; j < m.w1().columns(); j++)
+            m.w1()[i][j] = gauss(eng) / sqrt(m.features());
+    for(unsigned i = 0; i < m.w2().size(); i++)
+        m.w2()[i] = gauss(eng) / sqrt(m.neurons());
+}
+
+constexpr int batch_size = 10;
 constexpr double learning_rate = 1e-4;
-constexpr double dgamma = 0.99; // discount factor for reward
-constexpr double decay_rate = 0.99; // decay factor for RMSProp leaky sum of grad^2
+constexpr double dgamma = 0.99;
+constexpr double decay_rate = 0.99;
 
 constexpr double sigmoid(double x) {
     return 1 / (1 + exp(-x));
 }
 
-void discount_rewards(std::valarray<double> &r) {
+void discount_rewards(std::vector<double> &r) {
     double add = 0;
     for(int i = (int) r.size() - 1; i >= 0; i--) {
-        if(r[i]) add=0;
+        if(r[i])
+            add = 0;
         add = add * dgamma + r[i];
         r[i] = add;
     }
 }
-void normalize(std::valarray<double> &r) {
-    double mean = r.sum() / r.size();
+void normalize(Vector &r) {
+    double mean = sum(r) / r.size();
     r -= mean;
-    double stdev = std::sqrt(inner_product(r, r) / r.size());
+    double stdev = std::sqrt(sum_square(r) / r.size());
     if(stdev)
         r /= stdev;
 }
 
-Learning::Learning(int n, int f, seed_type seed) :
+Learning::Learning(unsigned n, unsigned f, seed_type seed) :
         m_neurons(n), m_features(f), m_model(n, f),
         m_gradbuf(n, f), m_rmsprop(n, f), m_engine(seed),
         m_episode(0), m_saved_feature(), m_saved_mid(),
         m_saved_pd(), m_saved_reward() {
-    m_model.randomize(m_engine);
+    randomize(m_model, m_engine);
 }
-bool Learning::play(std::valarray<double> feature) {
-    std::valarray<double> mid = m_model.w1() * feature;
-    for(int i = 0; i < m_neurons; i++)
+bool Learning::play(const std::vector<double> &orig_feature) {
+    Vector feature(orig_feature);
+    Vector mid = m_model.w1() * feature;
+    for(unsigned i = 0; i < m_neurons; i++)
         if(mid[i] < 0)
             mid[i] = 0;
 
     double p = sigmoid(inner_product(m_model.w2(), mid));
     bool action = std::bernoulli_distribution(p)(m_engine);
 
-    m_saved_feature.push_back(feature);
-    m_saved_mid.push_back(mid);
+    m_saved_feature.push_back(orig_feature);
+    m_saved_mid.push_back(std::vector<double>(mid));
     m_saved_pd.push_back((action ? 1.0 : 0.0) - p);
 
     return action;
 }
 void Learning::feedback(double reward) {
-    assert(reward >= -1.0 && reward <= 1.0);
     m_saved_reward.push_back(reward);
 }
 void Learning::game_over() {
     m_episode++;
 
-    Matrix<double> epx = to_matrix(m_saved_feature);
-    Matrix<double> eph = to_matrix(m_saved_mid);
-    std::valarray<double> epd = to_valarray(m_saved_pd);
-    std::valarray<double> epr = to_valarray(m_saved_reward);
+    discount_rewards(m_saved_reward);
 
-    discount_rewards(epr);
-    normalize(epr);
-    epd *= epr;
-
-    // policy backward
-    Matrix<double> dht(eph.columns(), epx.rows());
-    for(int i = 0; i < dht.rows(); i++)
-        for(int j = 0; j < dht.columns(); j++)
-            dht[i][j] = (eph[j][i] <= 0 ? 0 : epd[j] * m_model.w2()[i]);
-
-    m_gradbuf.w1() += dht * epx;
-    m_gradbuf.w2() += epd * eph;
-
-    // cleanup
+    Matrix epx(m_saved_feature);
+    Matrix eph(m_saved_mid);
+    Vector epd(m_saved_pd);
+    Vector epr(m_saved_reward);
     m_saved_feature.clear();
     m_saved_mid.clear();
     m_saved_pd.clear();
     m_saved_reward.clear();
 
+    normalize(epr);
+    epd *= epr;
+
+    // policy backward
+    Matrix dht(eph.columns(), epx.rows());
+    for(unsigned i = 0; i < dht.rows(); i++)
+        for(unsigned j = 0; j < dht.columns(); j++)
+            dht[i][j] = (eph[j][i] <= 0 ? 0 : epd[j] * m_model.w2()[i]);
+
+    m_gradbuf.w1() += dht * epx;
+    m_gradbuf.w2() += epd * eph;
+
     if(m_episode % batch_size == 0)
         batch_work();
 }
 
-void decay(double &lhs, double rhs) {
+inline void decay(double &lhs, double rhs) {
     lhs = lhs * decay_rate + rhs * rhs * (1 - decay_rate);
 }
-void decay(std::valarray<double> &lhs, const std::valarray<double> &rhs) {
+void decay(Vector &lhs, const Vector &rhs) {
     assert(lhs.size() == rhs.size());
-    for(int i = 0; i < (int) lhs.size(); i++)
+    for(unsigned i = 0; i < lhs.size(); i++)
         decay(lhs[i], rhs[i]);
 }
-void decay(Matrix<double> &lhs, const Matrix<double> &rhs) {
-    decay(lhs.data(), rhs.data());
+void decay(Matrix &lhs, const Matrix &rhs) {
+    assert(lhs.rows() == rhs.rows());
+    assert(lhs.columns() == rhs.columns());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        decay(lhs.data()[i], rhs.data()[i]);
 }
-void learn(double &lhs, double x, double y) {
+inline void learn(double &lhs, double x, double y) {
     assert(y >= 0.0);
     lhs += learning_rate * x / (std::sqrt(y) + 1e-5);
 }
-void learn(std::valarray<double> &lhs, const std::valarray<double> &x,
-        const std::valarray<double> &y) {
-    assert(lhs.size() == x.size());
-    assert(lhs.size() == y.size());
-    for(int i = 0; i < (int) lhs.size(); i++)
+void learn(Vector &lhs, const Vector &x, const Vector &y) {
+    assert(lhs.size() == x.size() && lhs.size() == y.size());
+    for(unsigned i = 0; i < lhs.size(); i++)
         learn(lhs[i], x[i], y[i]);
 }
-void learn(Matrix<double> &lhs, const Matrix<double> &x,
-        const Matrix<double> &y) {
-    learn(lhs.data(), x.data(), y.data());
+void learn(Matrix &lhs, const Matrix &x, const Matrix &y) {
+    assert(lhs.rows() == x.rows() && x.rows() == y.rows());
+    assert(lhs.columns() == x.columns() && x.columns() == y.columns());
+    for(unsigned i = 0; i < lhs.size(); i++)
+        learn(lhs.data()[i], x.data()[i], y.data()[i]);
 }
 void Learning::batch_work() {
     decay(m_rmsprop.w1(), m_gradbuf.w1());
@@ -220,7 +317,7 @@ void Learning::batch_work() {
     learn(m_model.w1(), m_gradbuf.w1(), m_rmsprop.w1());
     learn(m_model.w2(), m_gradbuf.w2(), m_rmsprop.w2());
 
-    m_gradbuf.reset();
+    reset(m_gradbuf);
 }
 
 std::istream &operator >> (std::istream &in, Model &x) {
@@ -229,26 +326,26 @@ std::istream &operator >> (std::istream &in, Model &x) {
         if(in >> s)
             z = stod(s);
     };
-    Matrix<double> &w1 = x.w1();
-    std::valarray<double> &w2 = x.w2();
+    Matrix &w1 = x.w1();
+    Vector &w2 = x.w2();
 
-    for(int i = 0; i < w1.rows(); i++)
-        for(int j = 0; j < w1.columns(); j++)
+    for(unsigned i = 0; i < w1.rows(); i++)
+        for(unsigned j = 0; j < w1.columns(); j++)
             gethex(w1[i][j]);
-    for(int i = 0; i < (int) w2.size(); i++)
+    for(unsigned i = 0; i < w2.size(); i++)
         gethex(w2[i]);
     return in;
 }
 std::ostream &operator << (std::ostream &out, const Model &x) {
-    const Matrix<double> &w1 = x.w1();
-    const std::valarray<double> &w2 = x.w2();
+    const Matrix &w1 = x.w1();
+    const Vector &w2 = x.w2();
 
     out << std::hexfloat;
-    for(int i = 0; i < w1.rows(); i++)
-        for(int j = 0; j < w1.columns(); j++)
+    for(unsigned i = 0; i < w1.rows(); i++)
+        for(unsigned j = 0; j < w1.columns(); j++)
             out << w1[i][j] << (j == w1.columns() - 1 ? '\n' : ' ');
-    for(int i = 0; i < (int) w2.size(); i++)
-        out << w2[i] << (i == (int) w2.size() - 1 ? '\n' : ' ');
+    for(unsigned i = 0; i < w2.size(); i++)
+        out << w2[i] << (i == w2.size() - 1 ? '\n' : ' ');
     return out << std::defaultfloat;
 }
 std::istream &operator >> (std::istream &in, Learning &x) {
